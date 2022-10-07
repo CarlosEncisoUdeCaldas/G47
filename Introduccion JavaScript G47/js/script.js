256499a6f8fa244b4757a782ac39568c397b8a89
CarlosEncisoUdeCaldas <carlosenciso.tic@ucaldas.edu.co>
2022-10-07 09:03:42 -0500

    aplicacion de script externo con funciones

---
+++ Introduccion JavaScript G47/js/script.js
@@ -0,0 +1,29 @@
+//console log de prueba
+console.log("Estoy dentro del archivo script.js");
+
+
+// funciones para mostrar y ocultar un elemento
+function mostrar(valor) {
+    
+    document.getElementById('mostrar').style.display = valor;
+
+}
+
+//funcion para cambiar el estilo
+function cambiarEstilo(size){
+    document.getElementById('estilo').style.fontSize = size;
+}
+
+//funcion para encender o apagar el bombillo
+function OnOff(rutaImagen) { 
+    document.getElementById('imagen').src = rutaImagen;
+}
+
+// funcion para contenido de una etiqueta
+function cambiarContenido() {
+    document.getElementById('demo').innerHTML = 'Contenido cambiado';
+}
+
+
+//insercion de texto a una etiqueta <p>
+document.getElementById("outline").innerHTML = "Insertando texto desde un Script fuera de linea";
